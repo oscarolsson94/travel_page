f732779328d706981b8e49416dd5c8c2082653ff
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-06 11:11:56 +0200

    starting navbar and install react-router-dom

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            TRAVEL
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
