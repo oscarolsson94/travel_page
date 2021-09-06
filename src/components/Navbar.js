d05445ffd25391cc9e1aa64d6bb8e30e635fbbb1
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-06 11:45:47 +0200

    adding links to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,10 @@ const Navbar = () => {
   const handleClick = () => {
     setClick(!click);
   };
+
+  const closeMobileMenu = () => {
+    setClick(false);
+  };
   return (
     <>
       <nav className="navbar">
@@ -17,6 +21,40 @@ const Navbar = () => {
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign up
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
