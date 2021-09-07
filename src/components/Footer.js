902167b0f87ad39b6eae186a084384591226bb16
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-07 18:12:59 +0200

    starting footer jsx

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="footer-container"></div>;
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the Adventure newsletter to receive our best vacation deals
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscribe at any time.
+        </p>
+        <div className="input-areas"></div>
+      </section>
+    </div>
+  );
 };
 
 export default Footer;
