04e04fde2f256bebfcf5a708224d2bf971f2e659
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-07 17:03:04 +0200

    passing props and rendering cards

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const CardItem = () => {
+const CardItem = ({ src, label, text, path }) => {
   return (
     <>
       <li className="cards__item">
-        <Link className="cards__item__link">
-          <figure className="cards__item__pic-wrap">
-            <img src="/" alt="Travel" className="cards__item__img" />
+        <Link className="cards__item__link" to={path}>
+          <figure className="cards__item__pic-wrap" data-category={label}>
+            <img src={src} alt="Travel" className="cards__item__img" />
           </figure>
           <div className="cards__item__info">
-            <h5 className="cards__item__text">heading</h5>
+            <h5 className="cards__item__text">{text}</h5>
           </div>
         </Link>
       </li>
