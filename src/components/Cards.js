0eaeeab2375e59369ffa103dcb77e644af837669
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-07 16:48:08 +0200

    basic JSX layout

---
+++ src/components/Cards.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Cards = () => {
+  return (
+    <div className="cards">
+      <h1>Check out out EPIC Destinations</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items"></ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
