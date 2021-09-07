840153cab8c78b8047e43784d9a96975b5fc6586
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-07 17:17:32 +0200

    adding routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
 
 function App() {
   return (
@@ -11,6 +13,12 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route exact path="/services">
+            <Services />
+          </Route>
+          <Route exact path="/products">
+            <Products />
           </Route>
         </Switch>
       </Router>
